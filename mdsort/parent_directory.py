7ccf07b59275c0595d6fefe852891aa58f4a202d
Monkey <Monkey@Tree.com>
2024-04-30 23:26:50 +0300

    created class ParentDirectory which will handle all series realted operations

--- mdsort/parent_directory.py
+++ mdsort/parent_directory.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass, field
+from functools import cached_property
 from pathlib import Path
 
+import formatter
 import settings
 
 
@@ -7,5 +10,24 @@ def find_parent_series_directories(series_root_directory: Path) -> set[str]:
     return {
         directory.name
         for directory in series_root_directory.iterdir()
-        if Path(directory / settings.PARENT_IDENTIFIER).exists()
+        if ParentDirectory.is_parent_directory(directory)
     }
+
+
+@dataclass
+class ParentDirectory:
+    path: Path
+    newly_assigned_files: list[Path] = field(default_factory=list)
+
+    @cached_property
+    def comparable_name(self):
+        return formatter.format_series_title_and_file_name(self.path.name)
+
+    @staticmethod
+    def is_parent_directory(directory: Path) -> bool:
+        return Path(directory / settings.PARENT_IDENTIFIER).exists()
+
+    def resolve_new_file_path(self, assigned_file: Path):
+        return self.path / formatter.format_series_filename_before_rename(
+            assigned_file.name, self.path.name
+        )
--- tests/test_parent_directory.py
+++ tests/test_parent_directory.py
@@ -10,7 +10,7 @@ import settings
 
 PARENT_IDENTIFIER = settings.PARENT_IDENTIFIER
 PARENT_SERIES_DIRECTORY_1 = "Mandalorian 2018"
-PARENT_SERIES_DIRECTORY_2 = "Avatar: The Last Airbender 2024 tt9018736"
+PARENT_SERIES_DIRECTORY_2 = "Avatar: The Last Airbender tt9018736"
 SERIES_DIRECTORY = "Catch 22"
 
 
@@ -37,3 +37,24 @@ def test_find_all_parent_series_directories(series_root_directory: Path) -> None
         PARENT_SERIES_DIRECTORY_1,
         PARENT_SERIES_DIRECTORY_2,
     }
+
+
+@pytest.fixture(scope="module")
+def parent_directory_2() -> parent_directory.ParentDirectory:
+    return parent_directory.ParentDirectory(
+        path=Path(PARENT_SERIES_DIRECTORY_2),
+        newly_assigned_files=[Path("Avatar The Last Airbender s01e02.mkv")],
+    )
+
+
+def test_parent_directory_comparable_name(parent_directory_2) -> None:
+    assert parent_directory_2.comparable_name == "avatar.the.last.airbender"
+
+
+def test_parent_directory_assigned_files_proper_path(parent_directory_2) -> None:
+    assert (
+        parent_directory_2.resolve_new_file_path(
+            parent_directory_2.newly_assigned_files[0]
+        )
+        == parent_directory_2.path / "Avatar: The Last Airbender - 01x02.mkv"
+    )
