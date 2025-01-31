5d123f2b6372e4957dca41b0142a0c4c27943bd8
Robert <zykon@pm.me>
2025-01-31 09:01:05 +0200

    finished sorter. not tested yet

--- mdsort/sorter.py
+++ mdsort/sorter.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 from pathlib import Path
 
 from mdsort.formatter import format_series_title_and_file_name
@@ -35,10 +36,9 @@ class Sorter:
 
     def sort(self) -> None:
         self.assign_all_media_to_parents()
-        # logging.info("Assignment completed")
         self.move_all_media_to_assigned_parents()
         self.move_unassigned_media_to_movies()
-        # logging.info("Sorting completed")
+        self.cleanup_empty_directories()
 
     def assign_all_media_to_parents(self) -> None:
         for parent_directory in self.series_parent_directories:
@@ -63,21 +63,22 @@ class Sorter:
                         parent_directory.newly_assigned_files.append(file)
                 self.assigned_directories.append(directory)
 
-    def move_unassigned_media_to_movies(self) -> None:
-        self.move_unassinged_files_to_movies()
-        self.moves_unassigned_directories_to_movies()
-
     def move_all_media_to_assigned_parents(self) -> None:
-        self.move_files_to_assigned_parents()
-        self.move_directories_to_assigned_parents()
+        for parent_directory in self.series_parent_directories:
+            for file in parent_directory.newly_assigned_files:
+                src = file
+                dst = parent_directory.resolve_new_file_path(file)
+                self.move(src, dst)
+
+    @staticmethod
+    def move(src: Path, dst: Path) -> None:
+        shutil.move(src, dst)
+        logging.info("Moved: %s", src)
+        logging.info("To: %s", dst)
 
     def move_unassigned_media_to_movies(self) -> None:
-        self.move_unassinged_files_to_movies()
-        self.moves_unassigned_directories_to_movies()
-
-    def move_all_media_to_assigned_parents(self) -> None:
-        self.move_files_to_assigned_parents()
-        self.move_directories_to_assigned_parents()
+        self.move_unassigned_files_to_movies()
+        self.move_unassigned_directories_to_movies()
 
     def is_all_downloaded_media_assigned(self) -> bool:
         return (
@@ -92,3 +93,20 @@ class Sorter:
     @property
     def unassigned_media_files(self) -> set[Path]:
         return set(self.media_files) - set(self.assigned_files)
+
+    def move_unassigned_files_to_movies(self) -> None:
+        for file in self.unassigned_media_files:
+            src = file
+            dst = self.movies_directory / src
+            self.move(src, dst)
+
+    def move_unassigned_directories_to_movies(self) -> None:
+        for directory in self.unassigned_media_directories:
+            src = directory
+            dst = self.movies_directory / src
+            self.move(src, dst)
+
+    def cleanup_empty_directories(self) -> None:
+        for directory in self.assigned_directories:
+            logging.info("Deleted %s", directory)
+            shutil.rmtree(directory)
